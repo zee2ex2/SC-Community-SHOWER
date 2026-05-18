66079dc9def5e006eeef1cf1eac02d6e85eb8551
zee2ex2 <repute_dingos.3k@icloud.com>
2026-05-18 15:59:50 -0700

    Trigger deploy

--- startup.sh
+++ startup.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 # Azure App Service startup script for SC SHOWER
-# All config is via environment variables set in the Azure portal
 
-# Install dependencies
 pip install -r requirements.txt
-
-# Start the server
 python server.py
